7956f0499fb23b442ee30c508402b199427231d1
@philshem <philshem@zoho.com>
2018-11-30 12:33:05 +0100

    add exception handling
    script can run 24/7 by handling exceptions and added sleep statements

--- scrape_to_pdf.py
+++ scrape_to_pdf.py
@@ -7,10 +7,39 @@ import config
 
 import pdfkit
 import os
+import glob
+from random import randint
+from time import sleep
+import datetime
+from interruptingcow import timeout
 
 urlbase = config.urlbase
 
-for x in xrange(config.min_page-1,config.max_page-1):
-	url = urlbase+str(x+1)
-	print(url)
-	pdfkit.from_url(url, 'raw'+os.sep+str(x+1).zfill(4)+'.pdf')
+existing_pdfs = glob.glob('raw'+os.sep+'*.pdf')
+#print existing_pdfs
+#exit(0)
+
+for x in xrange(config.min_page,config.max_page):
+	url = urlbase+str(x)
+	pdf_file = 'raw'+os.sep+str(x).zfill(4)+'.pdf'
+
+	if not os.path.exists(pdf_file):
+		print(datetime.datetime.now())
+		try:
+			with timeout(60*1, exception=RuntimeError):
+				while True:
+					pdfkit.from_url(url, pdf_file)
+					print('new pdf:',pdf_file)
+					sleep(randint(5,35))
+
+		except RuntimeError:
+			print('taking a small break')
+			sleep(60*1)
+			pass
+		except:
+			print('taking a big break')
+			sleep(60*10)
+			pass
+	else:
+		print('exists:',pdf_file)
+
